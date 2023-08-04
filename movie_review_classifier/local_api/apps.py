7f014e710d5a4cb0cc537db38a78e7754d122efb
defdet <116446387+defdet@users.noreply.github.com>
2023-08-04 14:06:25 +0300

    Delete movie_review_classifier/local_api directory

--- movie_review_classifier/local_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocalApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'local_api'
--- movie_review_classifier/local_api/urls.py
+++
@@ -1,8 +0,0 @@
-# local_api/urls.py
-
-from django.urls import path
-from .views import predict_rating
-
-urlpatterns = [
-    path('predict_rating/', predict_rating, name='predict_rating'),
-]
